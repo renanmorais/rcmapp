6e77ee4e37f29dd9e593ee06a7b2936f9520ad85
Renan Morais <arautog@gmail.com>
2015-06-28 17:13:28 -0300

    Init - criado estrutura para novo projeto

---
+++ script/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Renan on 28/06/2015.
+ */
+(function () {
+    'use strict';
+    /**
+     * Main module of the application.
+     */
+    angular
+        .module('rmAppApp', [
+            'ngAnimate',
+            'ngAria',
+            'ngCookies',
+            'ngMessages',
+            'ngResource',
+            'ngRoute',
+            'ngSanitize',
+            'ngTouch'
+        ])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/views/main.html',
+                    controller: 'MainCtrl'
+                })
+                .when('/about', {
+                    templateUrl: '/views/about.html',
+                    controller: 'AboutCtrl'
+                })
+                .when('/pessoa', {
+                    templateUrl: '/views/pessoa/pessoaPesquisa.html',
+                    controller: 'pessoaPesquisaController'
+                })
+                .when('/pessoa/cadastro', {
+                    templateUrl: '/views/pessoa/pessoaCasdastro.html',
+                    controller: 'pessoaCadastroController'
+                })
+                .when('/pessoa/pesquisa', {
+                    templateUrl: '/views/pessoa/pessoaPesquisa.html',
+                    controller: 'pessoaPesquisaController'
+                })
+
+                .otherwise({
+                    redirectTo: '/'
+                });
+        });
+})();
---
+++ views/about.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Renan on 28/06/2015.
+ */
+
+'use strict';
+
+angular.module('rmAppApp')
+    .controller('AboutCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
